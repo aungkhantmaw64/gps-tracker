797eb4fd0a729714afd3eb56aed6c9cb6c2c22e9
Aung Khant Maw <aungkhantmaw64@gmail.com>
2025-11-09 20:18:48 +0700

    network_manager: move common code to main

--- components/network_manager/network_manager.c
+++ components/network_manager/network_manager.c
@@ -107,16 +107,6 @@ static void network_manager_ip_event_cb(void *arg, esp_event_base_t event_base,
  ********************************************************************************/
 
 esp_err_t network_manager_init(void) {
-  esp_err_t ret = nvs_flash_init();
-  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    ret = nvs_flash_init();
-  }
-  ESP_ERROR_CHECK(ret);
-  ESP_ERROR_CHECK(esp_netif_init());
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
-
   g_net = MALLOC(sizeof(network_manager_t));
   g_net->event_group = xEventGroupCreate();
   g_net->netif = esp_netif_create_default_wifi_sta();
--- main/app_main.c
+++ main/app_main.c
@@ -1,10 +1,22 @@
+#include "esp_event.h"
+#include "esp_netif.h"
 #include "freertos/FreeRTOS.h"
 #include "network_manager.h"
+#include "nvs_flash.h"
 #include "payload.h"
 
 #include <stdbool.h>
 
 void app_main(void) {
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
   ESP_ERROR_CHECK(network_manager_init());
   ESP_ERROR_CHECK(payload_init());
   while (true) {
